821d1456f476e38e67a9b99d7fdcd7457ac1ff8e
Poscat <poscat@mail.poscat.moe>
2020-06-08 22:41:46 +0800

    Export some generated types

--- src/TDLib/Types.hs
+++ src/TDLib/Types.hs
@@ -1,1 +1,35 @@
-module TDLib.Types where
+module TDLib.Types
+  ( Error,
+    Ok,
+    TdlibParameters,
+    Update,
+
+    -- * Messages
+    Message,
+    Messages,
+    FoundMessages,
+    DraftMessage,
+
+    -- * Users
+    User,
+    Users,
+    UserType,
+    UserFullInfo,
+    UserProfilePhoto,
+    UserProfilePhotos,
+
+    -- * Chats
+    Chat,
+    ChatType,
+    ChatMember,
+    ChatMembers,
+    BasicGroup,
+    BasicGroupFullInfo,
+    Supergroup,
+    SupergroupFullInfo,
+    SecretChat,
+    SecretChatState,
+  )
+where
+
+import TDLib.Generated
